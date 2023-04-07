e6a877201c436dd0c1792fe84ba5c1013be5bfa3
LeandroGir <112043796+LeandroGir@users.noreply.github.com>
2023-04-07 15:56:26 -0400

    Initial commit

---
+++ Prueba/conexion.php
@@ -0,0 +1,8 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$database="geografia_ch";
+// Conexion a la base de datos
+$conexion=mysqli_connect($servername,$username,$password,$database);
+?>
